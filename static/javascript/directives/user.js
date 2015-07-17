6abbc611260a26df0c33d7da20122b9af1fb558b
Christopher Clark <chrisdclark3@gmail.com>
2015-07-16 20:31:41 -0700

    http nonsense

--- static/javascript/directives/user.js
+++ static/javascript/directives/user.js
@@ -3,7 +3,7 @@ app.factory('UsersFactory', function ($http) {
   var factory = {};
 
   factory.get_users = function (callback) {
-    $http.get('https://puppals-api.heroku-app.com/users').success(function (res) {
+    $http.get('//puppals-api.heroku-app.com/users').success(function (res) {
       callback(res);
     });
   };
@@ -11,7 +11,7 @@ app.factory('UsersFactory', function ($http) {
   factory.login = function (current_user, callback) {
     $http({
       method: 'POST',
-      url: 'https://puppals-api.heroku-app.com/sessions',
+      url: '//puppals-api.heroku-app.com/sessions',
       params: current_user,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     }).success(function (res) {
