ff166d721a328ea9bb54d28aa65bc18d9cd449b4
Christopher Clark <chrisdclark3@gmail.com>
2015-07-18 03:11:56 -0700

    updated some css related properties and am trying new message

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ io.sockets.on('connection', function (socket) {
   socket.on('get_conversations', function (data) {
     console.log("REQUESTING DATA FROM API IN GET CONVERSATIONS", data);
     request({
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Control-Allow-Origin': '*'
-      },
       uri: "//puppals-api.herokuapp.com/conversations/" + data.current_user.id,
       method: "GET",
       json: true
@@ -32,10 +28,6 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('send_message', function (data) {
     request({
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Control-Allow-Origin': '*'
-      },
       uri: "//puppals-api.herokuapp.com/messages/",
       method: "POST",
       json: data,
